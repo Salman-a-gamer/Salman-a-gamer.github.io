cbe6b728edfc22b1238539a3c40e2ca48e25c93b
Salman-a-gamer <68333931+Salman-a-gamer@users.noreply.github.com>
2024-07-01 18:26:16 +0530

    Add files via upload

---
+++ JSforLEARN.js
@@ -0,0 +1,12 @@
+alert("last Week");
+
+function Theme() {
+	// body...
+	alert("billy!!!");
+	document.getElementById("mainhead").style.color="#FFFFFF";
+	// document.getElementById("mainhead").style.backgroundColor="#FFFFFF";
+	// document.getElementById("mainhead").style.LetterSpacing="15px";
+	// document.getElementById("mainhead").removeProperty("background");
+	alert("see");
+
+}
